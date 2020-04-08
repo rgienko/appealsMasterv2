8ecc6b819bdb77a2874c4904ce8504dbb66c78cf
Randall <Gienko>
2020-04-08 15:03:39 -0500

    Created Django App - app Set-Up Templates Folder Added layout.html, index.html

--- app/urls.py
+++ app/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('dashboard/', include('app.urls')),
-    path('admin/', admin.site.urls),
+    path('dashboard/', views.index, name='index'),
 ]
