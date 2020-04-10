7dd8342b55076266e4969361d2c6c40fe75abede
Randall <Gienko>
2020-04-10 15:11:02 -0500

    Models ha

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-@admin.register(action_master)
-
-class action_master_admin(admin.ModelAdmin):
-    list_display = ('id','note','description', 'lead_time', 'type')
